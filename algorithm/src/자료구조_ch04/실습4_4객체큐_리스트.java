7367e05314de01b795036eb69ca81443221a23ab
inayong <141033984+inayong@users.noreply.github.com>
2023-09-11 14:06:27 +0900

    객체큐 (리스트)

--- algorithm/src/자료구조_ch04/실습4_4객체큐_리스트.java
+++ algorithm/src/자료구조_ch04/실습4_4객체큐_리스트.java
@@ -1,151 +1,185 @@
-//package 자료구조_ch04;
-//
-//import java.util.Random;
-//import java.util.Scanner;
-//
-///*
-// * Queue of ArrayList of Point
-// */
-//import java.util.ArrayList;
-//import java.util.List;
-//
-////int형 고정 길이 큐
-//class objectQueue {
-//
-////	private int[] que; // 큐용 배열 - 교재 버젼
-//    private List<Point> que;
-//	//private List<Integer> que; // 수정본
-//	private int capacity; // 큐의 크기
-//	private int front; // 맨 처음 요소 커서
-//	private int rear; // 맨 끝 요소 커서
-//	private int num; // 현재 데이터 개수
-//
-////--- 실행시 예외: 큐가 비어있음 ---//
-//	public class EmptyQueueException extends RuntimeException {
-//		public EmptyQueueException() {
-//		}
-//	}
-//
-////--- 실행시 예외: 큐가 가득 찼음 ---//
-//	public class OverflowQueueException extends RuntimeException {
-//		public OverflowQueueException() {
-//		}
-//	}
-//
-////--- 생성자(constructor) ---//
-//public objectQueue(int maxlen) {
-//	//구현
-//	num = front = rear = 0;
-//	capacity = maxlen;
-//	try {
-//		que = new ArrayList<Point>(capacity);
-//	} catch (OutOfMemoryError e) {
-//		capacity = 0;
-//	}
-//}
-//
-////--- 큐에 데이터를 인큐 ---//
-//	public int enque(Point x) throws OverflowQueueException {
-//		//구현
-//	}
-//
-////--- 큐에서 데이터를 디큐 ---//
-//	public Point deque() throws EmptyQueueException {
-//		//구현
-//	}
-//
-////--- 큐에서 데이터를 피크(프런트 데이터를 들여다봄) ---//
-//	public Point peek() throws EmptyQueueException {
-//		//구현
-//	}
-//
-////--- 큐를 비움 ---//
-//	public void clear() {
-//		num = front = rear = 0;
-//	}
-//
-////--- 큐에서 x를 검색하여 인덱스(찾지 못하면 –1)를 반환 ---//
-//	public int indexOf(Point x) {
-//		//구현
-//	}
-//
-////--- 큐의 크기를 반환 ---//
-//	public int getCapacity() {
-//		return capacity;
-//	}
-//
-////--- 큐에 쌓여 있는 데이터 개수를 반환 ---//
-//	public int size() {
-//		return num;
-//	}
-//
-////--- 큐가 비어있는가? ---//
-//	public boolean isEmpty() {
-//		return num <= 0;
-//	}
-//
-////--- 큐가 가득 찼는가? ---//
-//	public boolean isFull() {
-//		return num >= capacity;
-//	}
-//
-////--- 큐 안의 모든 데이터를 프런트 → 리어 순으로 출력 ---//
-//	public void dump() {
-//		//구현
-//	}
-//}
-//public class 실습4_4객체큐_리스트 {
-//	public static void main(String[] args) {
-//		Scanner stdIn = new Scanner(System.in);
-//		objectQueue s = new objectQueue(4); // 최대 64개를 인큐할 수 있는 큐
-//		Random random = new Random();
-//		int rndx = 0, rndy = 0;
-//		Point p = null;
-//		while (true) {
-//			System.out.println(" "); // 메뉴 구분을 위한 빈 행 추가
-//			System.out.printf("현재 데이터 개수: %d / %d\n", s.size(), s.getCapacity());
-//			System.out.print("(1)인큐　(2)디큐　(3)피크　(4)덤프　(0)종료: ");
-//
-//			int menu = stdIn.nextInt();
-//			if (menu == 0)
-//				break;
-//
-//			int x;
-//			switch (menu) {
-//			case 1: // 인큐
-//				System.out.print("데이터: ");
-//				rndx = random.nextInt(20);
-//				rndy = random.nextInt(20);
-//				p = new Point(rndx,rndy);
-//				try {
-//					s.enque(p);
-//				} catch(objectQueue.OverflowQueueException e) {
-//					System.out.println("stack이 가득찼있습니다.");
-//				}
-//				break;
-//
-//			case 2: // 디큐
-//				try {
-//					p = s.deque();
-//					System.out.println("디큐한 데이터는 " + p + "입니다.");
-//				} catch (objectQueue.EmptyQueueException e) {
-//					System.out.println("큐가 비어 있습니다.");
-//				}
-//				break;
-//
-//			case 3: // 피크
-//				try {
-//					p = s.peek();
-//					System.out.println("피크한 데이터는 " + p + "입니다.");
-//				} catch (objectQueue.EmptyQueueException e) {
-//					System.out.println("큐가 비어 있습니다.");
-//				}
-//				break;
-//
-//			case 4: // 덤프
-//				s.dump();
-//				break;
-//			}
-//		}
-//	}
-//}
+package 자료구조_ch04;
+
+import java.util.Random;
+import java.util.Scanner;
+
+import 자료구조_ch04.Queue2.EmptyQueueException;
+
+import java.awt.Point;
+/*
+ * Queue of ArrayList of Point
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+//int형 고정 길이 큐
+class objectQueue {
+
+//	private int[] que; // 큐용 배열 - 교재 버젼
+    private List<Point> que;
+	//private List<Integer> que; // 수정본
+	private int capacity; // 큐의 크기
+	private int front; // 맨 처음 요소 커서
+	private int rear; // 맨 끝 요소 커서
+	private int num; // 현재 데이터 개수
+
+//--- 실행시 예외: 큐가 비어있음 ---//
+	public class EmptyQueueException extends RuntimeException {
+		public EmptyQueueException() {
+		}
+	}
+
+//--- 실행시 예외: 큐가 가득 찼음 ---//
+	public class OverflowQueueException extends RuntimeException {
+		public OverflowQueueException() {
+		}
+	}
+
+//--- 생성자(constructor) ---//
+public objectQueue(int maxlen) {
+	//구현
+	num = front = rear = 0;
+	capacity = maxlen;
+	try {
+		que = new ArrayList<Point>(capacity);
+	} catch (OutOfMemoryError e) {
+		capacity = 0;
+	}
+}
+
+//--- 큐에 데이터를 인큐 ---//
+	public int enque(Point x) throws OverflowQueueException {
+		//구현
+		System.out.println("num = " + num);
+		if (num >= capacity)
+			throw new OverflowQueueException();
+		que.add(x);
+		rear++;
+		num++;
+		return 1;
+	}
+
+//--- 큐에서 데이터를 디큐 ---//
+	public Point deque() throws EmptyQueueException {
+		//구현
+		if (num <= 0)
+			throw new EmptyQueueException();
+		Point p = que.get(0);
+		que.remove(front);
+		rear--;
+		num--;
+		return p;
+	}
+
+//--- 큐에서 데이터를 피크(프런트 데이터를 들여다봄) ---//
+	public Point peek() throws EmptyQueueException {
+		//구현
+		if (num <= 0)
+			throw new EmptyQueueException();
+		return que.get(front);
+	}
+
+//--- 큐를 비움 ---//
+	public void clear() {
+		num = front = rear = 0;
+	}
+
+//--- 큐에서 x를 검색하여 인덱스(찾지 못하면 –1)를 반환 ---//
+	public int indexOf(Point x) {
+		//구현
+		for (int i = 0; i < num; i++) {
+			int idx = (i + front) % capacity;
+			if (que.get(idx).equals(x))
+				return idx;
+		}
+		return -1;
+	}
+
+//--- 큐의 크기를 반환 ---//
+	public int getCapacity() {
+		return capacity;
+	}
+
+//--- 큐에 쌓여 있는 데이터 개수를 반환 ---//
+	public int size() {
+		return num;
+	}
+
+//--- 큐가 비어있는가? ---//
+	public boolean isEmpty() {
+		return num <= 0;
+	}
+
+//--- 큐가 가득 찼는가? ---//
+	public boolean isFull() {
+		return num >= capacity;
+	}
+
+//--- 큐 안의 모든 데이터를 프런트 → 리어 순으로 출력 ---//
+	public void dump() {
+		//구현
+		if (num <= 0)
+			System.out.println("큐가 비어 있습니다.");
+		else {
+			for (int i = 0; i < num; i++)
+//				System.out.println(que[(i + front) % capacity] + " ");
+				System.out.println(que.get(i) ); 
+			System.out.println();
+		}
+	}
+}
+public class 실습4_4객체큐_리스트 {
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		objectQueue s = new objectQueue(4); // 최대 64개를 인큐할 수 있는 큐
+		Random random = new Random();
+		int rndx = 0, rndy = 0;
+		Point p = null;
+		while (true) {
+			System.out.println(" "); // 메뉴 구분을 위한 빈 행 추가
+			System.out.printf("현재 데이터 개수: %d / %d\n", s.size(), s.getCapacity());
+			System.out.print("(1)인큐　(2)디큐　(3)피크　(4)덤프　(0)종료: ");
+
+			int menu = stdIn.nextInt();
+			if (menu == 0)
+				break;
+
+			int x;
+			switch (menu) {
+			case 1: // 인큐
+				System.out.print("데이터: ");
+				rndx = random.nextInt(20);
+				rndy = random.nextInt(20);
+				p = new Point(rndx,rndy);
+				try {
+					s.enque(p);
+				} catch(objectQueue.OverflowQueueException e) {
+					System.out.println("stack이 가득찼있습니다.");
+				}
+				break;
+
+			case 2: // 디큐
+				try {
+					p = s.deque();
+					System.out.println("디큐한 데이터는 " + p + "입니다.");
+				} catch (objectQueue.EmptyQueueException e) {
+					System.out.println("큐가 비어 있습니다.");
+				}
+				break;
+
+			case 3: // 피크
+				try {
+					p = s.peek();
+					System.out.println("피크한 데이터는 " + p + "입니다.");
+				} catch (objectQueue.EmptyQueueException e) {
+					System.out.println("큐가 비어 있습니다.");
+				}
+				break;
+
+			case 4: // 덤프
+				s.dump();
+				break;
+			}
+		}
+	}
+}
